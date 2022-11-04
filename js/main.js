6f96e2524649120542e85896533573be34e6bd2f
ludirez <diazrestrepo1@gmail.com>
2022-11-04 17:29:20 -0500

    integrando menu hamburguesa mobile, tachando html 8

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const menuEmailUser = document.querySelector('.navbar-email')
 const showUserMenu = document.querySelector('.desktop-menu')
+const listenerBurgerMenu = document.querySelector('.menu')
+const burgerMenu = document.querySelector('.mobile-menu')
 
+function toggleBurgerMenu() {
+    burgerMenu.classList.toggle('inactive')
+}
 function toggleUserMenu(){
     showUserMenu.classList.toggle('inactive')
 }
 
 menuEmailUser.addEventListener('click', toggleUserMenu)
+listenerBurgerMenu.addEventListener('click', toggleBurgerMenu)
