23c02335f22176320a6f33ff6e0c2f4e447aacc9
ludirez <diazrestrepo1@gmail.com>
2022-11-05 18:18:22 -0500

    agregado el aside de productos. renombrando clase 13

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
-const menuEmailUser = document.querySelector('.navbar-email')
-const showUserMenu = document.querySelector('.desktop-menu')
-const listenerBurgerMenu = document.querySelector('.menu')
-const burgerMenu = document.querySelector('.mobile-menu')
+const emailMenuUser = document.querySelector('.navbar-email')
+const toggleMenuUser = document.querySelector('.desktop-menu')
 
-function toggleBurgerMenu() {
-    burgerMenu.classList.toggle('inactive')
-}
-function toggleUserMenu(){
-    showUserMenu.classList.toggle('inactive')
+const dropdownMobileMenu = document.querySelector('.menu')
+const toggleDropdownMobileMenu = document.querySelector('.mobile-menu')
+
+const shoppingCartDropdown = document.querySelector('.navbar-shopping-cart')
+const toggleShoppingCartDropdown = document.querySelector('.product-detail')
+
+const aryInactiveElements = []
+
+function toggleUserMenu(){//método 1. forzando ocultar las otras 2
+    toggleDropdownMobileMenu.classList.add('inactive')
+    toggleShoppingCartDropdown.classList.add('inactive')
+    toggleMenuUser.classList.toggle('inactive')
 }
 
-menuEmailUser.addEventListener('click', toggleUserMenu)
-listenerBurgerMenu.addEventListener('click', toggleBurgerMenu)
+function toggleMobileMenu() {
+    toggleMenuUser.classList.add('inactive')
+    toggleShoppingCartDropdown.classList.add('inactive')
+    toggleDropdownMobileMenu.classList.toggle('inactive')
+}
+
+function toggleShoppingCartMenu() {
+    toggleMenuUser.classList.add('inactive')
+    toggleDropdownMobileMenu.classList.add('inactive')
+    toggleShoppingCartDropdown.classList.toggle('inactive')
+}
+
+
+emailMenuUser.addEventListener('click', toggleUserMenu)
+dropdownMobileMenu.addEventListener('click', toggleMobileMenu)
+shoppingCartDropdown.addEventListener('click', toggleShoppingCartMenu)
+
