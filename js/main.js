1973bf0d3cacd264a1e9bfd0976336d11b319390
ludirez <diazrestrepo1@gmail.com>
2022-11-07 10:04:40 -0500

    lista de productos agregada

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ const toggleDropdownMobileMenu = document.querySelector('.mobile-menu')
 const shoppingCartDropdown = document.querySelector('.navbar-shopping-cart')
 const toggleShoppingCartDropdown = document.querySelector('.product-detail')
 
+const cardsContainers = document.querySelector('.cards-container')
+
+
 const aryInactiveElements = []
 
 function toggleUserMenu(){//método 1. forzando ocultar las otras 2
@@ -32,3 +35,59 @@ emailMenuUser.addEventListener('click', toggleUserMenu)
 dropdownMobileMenu.addEventListener('click', toggleMobileMenu)
 shoppingCartDropdown.addEventListener('click', toggleShoppingCartMenu)
 
+//Creando Product List
+
+const productList = []
+productList.push({
+  productName: 'Bike',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  textAlt: 'Imagen de producto',
+})
+productList.push({
+  productName: 'House',
+  price: 120,
+  image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+  textAlt: 'Imagen de producto',
+})
+productList.push({
+  productName: 'Car',
+  price: 120,
+  image: '',
+  textAlt: 'Imagen de producto',
+})
+
+function renderProducts(arr) {
+  for (product of arr){
+    const productoCard = document.createElement('div')
+    productoCard.classList.add('product-card')
+    
+        const productImg = document.createElement('img') //product = {name, price, image} -> product.image
+          productImg.setAttribute('src', product.image)
+          productImg.setAttribute('alt', product.textAlt)
+
+        const productInfo = document.createElement('div')
+              productInfo.classList.add('product-info')
+        
+            const productDivInfo = document.createElement('div')
+                const priceInfo = document.createElement('p')
+                      priceInfo.innerText = '$' + product.price
+                const titleInfo = document.createElement('p')
+                      titleInfo.innerText = product.productName
+            productDivInfo.append(priceInfo, titleInfo)
+
+            const productFigure = document.createElement('figure')
+                const imgFigure = document.createElement('img')
+                      imgFigure.setAttribute('src', './img/icons/bt_add_to_cart.svg')
+                      imgFigure.setAttribute('alt', 'icono')
+            productFigure.append(imgFigure)
+                      
+        productInfo.append(productDivInfo, productFigure)
+
+    productoCard.append(productImg, productInfo)
+cardsContainers.appendChild(productoCard)
+}
+}
+
+renderProducts(productList)
+// -----
